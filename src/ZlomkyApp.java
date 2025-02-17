0936c16b93e24a74714fe75887f30230bbb8a882
Hary04-U <rehakda1@uhk.cz>
2025-02-17 11:26:37 +0100

    Aritmetic operations in Zlomek

--- src/ZlomkyApp.java
+++ src/ZlomkyApp.java
@@ -3,10 +3,18 @@ import cz.uhk.zlomky.model.Zlomek;
 public class ZlomkyApp {
     public static void main(String[] args) {
         System.out.println("Zlomkoaplikace");
-        Zlomek a = new Zlomek(1,2);
+        Zlomek a = new Zlomek(3,4);
+        Zlomek b = new Zlomek(2,5);
+        Zlomek c = a.krat(b).zkratit();
+        Zlomek d = a.deleno(b).zkratit();
+        Zlomek e = a.scitani(b);
+        Zlomek f = a.odcitani(b);
 //        System.out.println(" "+a.getCitatel());
 //        System.out.println("---");
 //        System.out.println(" "+a.getJmenovatel());
-        System.out.println(a.toString());
+        System.out.println(c.toString());
+        System.out.println(d.toString());
+        System.out.println(e.toString());
+        System.out.println(f.toString());
     }
 }
--- src/cz/uhk/zlomky/model/Zlomek.java
+++ src/cz/uhk/zlomky/model/Zlomek.java
@@ -17,6 +17,39 @@ public class Zlomek {
         return jmenovatel;
     }
 
+    public Zlomek krat(Zlomek druhy){
+        return new Zlomek(citatel * druhy.citatel, jmenovatel * druhy.jmenovatel);
+    }
+
+    public Zlomek deleno(Zlomek druhy){
+        return new Zlomek(citatel * druhy.jmenovatel, jmenovatel * druhy.citatel);
+    }
+
+    public Zlomek scitani(Zlomek druhy) {
+        return new Zlomek (citatel * druhy.jmenovatel + druhy.citatel * jmenovatel, jmenovatel * druhy.jmenovatel);
+    }
+
+    public Zlomek odcitani(Zlomek druhy) {
+        return new Zlomek (citatel * druhy.jmenovatel - druhy.citatel * jmenovatel, jmenovatel * druhy.jmenovatel);
+    }
+
+    public Zlomek zkratit() {
+        int a = Math.abs(citatel);
+        int b = Math.abs(jmenovatel);
+        if (a < b) {
+            int pomoc = a;
+            a = b;
+            b = pomoc;
+        }
+        int zb;
+        do {
+            zb = a % b;
+            a = b;
+            b = zb;
+        } while (zb != 0);
+        return new Zlomek(citatel/a, jmenovatel/a);
+    }
+
     @Override
     public String toString() {
 //        return " " + citatel + "\n" + "---" + "\n" + " " + jmenovatel;
