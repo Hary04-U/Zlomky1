e3d6755dba31e577784778d355ed2549398fd7ee
Hary04-U <rehakda1@uhk.cz>
2025-03-07 09:14:06 +0100

    Přidání dalších testů

--- src/cz/uhk/zlomky/model/SeznamZlomkuTest.java
+++ src/cz/uhk/zlomky/model/SeznamZlomkuTest.java
@@ -73,14 +73,6 @@ class SeznamZlomkuTest {
 
     @org.junit.jupiter.api.Test
     void vypisZlomky() {
-        seznam.pridatZlomek(new Zlomek(3,4));
-        seznam.pridatZlomek(new Zlomek(1,2));
-        List<Zlomek> z = seznam.ziskejZlomky();
-        Zlomek z1 = seznam.vratZlomek(0);
-        Zlomek z2 = seznam.vratZlomek(1);
-        assertEquals(z1.getCitatel(), z.get(0).getCitatel(), "Citatel1");
-        assertEquals(z1.getJmenovatel(), z.get(0).getJmenovatel(), "Jmenovatel1");
-        assertEquals(z2.getCitatel(), z.get(1).getCitatel(), "Citatel2");
-        assertEquals(z2.getJmenovatel(), z.get(1).getJmenovatel(), "Jmenovatel2");
+
     }
 }
